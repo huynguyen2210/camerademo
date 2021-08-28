6920ddba578ded794eefc99d39c3ab4db5cfab96
huynguyen2210 <huynguyen2210@gmail.com>
2021-08-28 17:29:15 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,24 @@ $('.banner').slick({
 
     ]
 });
+$('.videoSystem__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // fade: true,
+    arrows: false,
+    dots: true,
+    responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+});
