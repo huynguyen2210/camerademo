c7437d243cc9ad2e545b0102f26d859c8bccc3af
huynguyen2210 <huynguyen2210@gmail.com>
2021-08-31 10:32:22 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@ $('.banner').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
     fade: true,
+    cssEase: 'linear',
     arrows: false,
     dots: true,
     responsive: [{
@@ -34,4 +36,53 @@ $('.videoSystem__slider').slick({
             }
         }
     ]
+});
+$('.caseStudy__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // fade: true,
+    arrows: false,
+    dots: true,
+    responsive: [{
+        //     breakpoint: 992,
+        //     settings: {
+        //         slidesToShow: 2
+        //     }
+        // },
+        // {
+        //     breakpoint: 576,
+        //     settings: {
+        //         slidesToShow: 1
+        //     }
+    }]
+});
+$('.caseStudy-album').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.caseStudy-album-nav',
+    responsive: [{
+        //     breakpoint: 992,
+        //     settings: {
+        //         slidesToShow: 2
+        //     }
+        // },
+        // {
+        //     breakpoint: 576,
+        //     settings: {
+        //         slidesToShow: 1
+        //     }
+    }]
+});
+
+$('.caseStudy-album-nav').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    asNavFor: '.caseStudy-album',
+
 });
